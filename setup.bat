96acddff2abff08941cc874df848ba333aea1353
yuhkix <yuhkixx@protonmail.com>
2026-03-29 08:13:05 +0200

    chore: add setup.bat for cloning vendor deps, track crunch headers in repo

---
+++ setup.bat
@@ -0,0 +1,30 @@
+@echo off
+echo === Prophecy - Dependency Setup ===
+echo.
+
+if not exist vendor mkdir vendor
+
+echo [1/3] Cloning ImGui (docking branch)...
+if not exist vendor\imgui (
+    git clone --depth 1 --branch docking https://github.com/ocornut/imgui.git vendor/imgui
+) else (
+    echo Already exists, skipping.
+)
+
+echo [2/3] Cloning LZMA SDK...
+if not exist vendor\lzma (
+    git clone --depth 1 https://github.com/jljusten/LZMA-SDK.git vendor/lzma
+) else (
+    echo Already exists, skipping.
+)
+
+echo [3/3] Cloning Native File Dialog Extended...
+if not exist vendor\nfd (
+    git clone --depth 1 https://github.com/btzy/nativefiledialog-extended.git vendor/nfd
+) else (
+    echo Already exists, skipping.
+)
+
+echo.
+echo === Setup complete ===
+echo Run: xmake f -p windows ^&^& xmake build
